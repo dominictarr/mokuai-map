eef1bb1cfa62bdb7b5b99b1943e0f6dc7e85de20
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-08 12:11:17 +0800

    working but buggy

--- index.js
+++ index.js
@@ -1,92 +1,85 @@
 var h    = require('hyperscript')
 var Vec2 = require('vec2')
-var vdom = require('vec2-dom')
 var View = require('vec2-view')
 
-function Layer (zoom) {
-  this.zoom = zoom
-  this.tiles = {}
+var NOTE = h('pre', {style: {
+    position: 'fixed',
+    'z-index': 30,
+    left:'20px',
+    top:'20px'
+}})
+
+var Layer = require('./layer')
+
+document.body.appendChild(NOTE)
+
+function round (v) {
+  v.set(Math.round(v.x), Math.round(v.y))
+  return v
 }
 
-var l = Layer.prototype
 
 module.exports = function () {
 
-  var div  = h('div.vec2-map', {style: {overflow: 'hidden'}})
-  var tiles = {}
+  var layers = []
 
-  function getTile(x, y, z, cb) {
-    var id = x+':'+y
-    if(tiles[id]) return false
+  //fix how the divs work... tiles should be in layers
+  //and should be layed ontop of each other (more detailed ontop of less)
+  
+  var div = h('div', {style: {
+      //these settings will make the map appear within the box
+      //so that it's embedable.
+      //TODO: make the dimensions a parameter
+      width: window.innerWidth + 'px',
+      height: window.innerHeight + 'px',
+      'overflow': 'hidden',
+      'position': 'relative',
+      'z-index': 20
+    }})
 
-    var src = 'https://khms1.google.com/kh/v=134&src=app'
-            + '&x=' + x
-            + '&y=' + y
-            + '&z=' + z
-            + '&s=G'
-    ;
+    for(var i = 1; i < 8; i ++) {
+      var l = new  Layer(i)
+      layers.push(l)
+      div.appendChild(l.div)
+    }
 
-    var img = h('img', {src: src, style: {'pointer-events': 'none'}})
-    div.appendChild(img)
-    var r = vdom.absolute(img, true)
-
-    img.screenOrigin = r
-    tiles[id] = img
-
-    if(cb)
-      img.addEventListener('load', function (e) {
-        cb(null, img)
-      })
-
-    img.origin = new Vec2(x, y)
-    return img
-  }
-
-  function round (v) {
-    v.set(Math.round(v.x), Math.round(v.y))
-    return v
-  }
-
-  var view = new View().track(div)
+  var view = new View().track(document.body)
   view.view.set(window.innerWidth, window.innerHeight)
   view.zoom(256)
-  var Z = 1
 
   //get screen dimensions
   //get the screen in the model
 
   view.screenModel = new Vec2()
 
-  var screenSize = view.view.divide(view.zoom(), true)
+  var screenSize = view.view.divide(view.zoom()*2, true)
   var screenMin = view.center.add(screenSize, true)
   var screenMax = view.center.subtract(screenSize, true)
 
-  view.change(function (e) {
-    screenSize.set(view.view).divide(view.zoom())
+   view.change(function (e) {
+    var z = Math.min(
+      Math.floor(Math.log(view.zoom()/256)/Math.LN2) + 1,
+      layers.length - 2)
+    var z2 = z + 1
+    
+    //TODO: refactor this out 
+    screenSize.set(view.view).divide(view.zoom()*2)
     screenMax.set(view.center).add(screenSize)
     screenMin.set(view.center).subtract(screenSize)
 
-    var mX = Math.max(Math.floor(screenMin.x), 0)
-    var mY = Math.max(Math.floor(screenMin.y), 0)
-    var MX = Math.min(Math.floor(screenMax.x), Math.pow(2, Z) - 1)
-    var MY = Math.min(Math.floor(screenMax.y), Math.pow(2, Z) - 1)
+    var scale = Math.pow(2, z)
 
-    //console.log('MAX',  Math.pow(2, Z))
+  NOTE.textContent = JSON.stringify([/*screenSize, screenMin, screenMax,*/ z, scale, [mX, mY, MX, MY]], null, 2)
 
-    for(var i =  mX; i <= MX; i++)
-      for(var j =  mY; j <= MY; j++)
-        if(getTile(i, j, Z)) console.log(i, j)
-    
-
-    for(var k in tiles) {
-      var img = tiles[k]
-      img.screenOrigin.set(round(view.toView(img.origin)))
-      img.style.width = Math.floor(view.zoom()) + 'px'
+    //show two layers at once (so you can see larger layer through gaps)
+    if(layers[z2] && layers[z]){
+      layers[z].update(screenMin, screenMax, view)
+      layers[z2].update(screenMin, screenMax, view)
+      layers.forEach(function (e, i) {
+        e.show(i == z || i == z2)
+      })
     }
-
-    var z = Math.log(view.zoom()/256)/Math.LN2
-    console.log(z + 1)
-
   })
+
   return div
 }
